ce6dfcbac411760ea9d8a416f3b91781b93af792
Zaryan17 <k.sarmadzaryan.geekslogicity@gmail.com>
2023-04-19 00:05:49 +0500

    changes

--- src/screens/App/Order.js
+++ src/screens/App/Order.js
@@ -18,22 +18,58 @@ import {Images} from '../../assets/images';
 import Input from '../../components/Input/index';
 import axios from '../../utils/axios';
 import BaseURL from '../../constants/apiEndPoints';
-import Loader from '../../components/Loader.component';
 import ReturnLog from '../../components/ReturnLog/index';
 import Header from '../../components/Header';
 import MyStatusBar from '../../components/StatusBar';
 
+import Loader from '../../components/Loader.component';
+
 //third party library
 import {useSelector, useDispatch} from 'react-redux';
+import moment from 'moment';
 
 const Index = ({navigation, ...props}) => {
   const [modal, setModal] = useState(false);
+  const [isLoader, setIsLoader] = useState(false);
+  const [data, setData] = useState([]);
+
+  const user = useSelector(state => state.userReducer.userData);
+  console.log('user ==>', user);
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ` + user?.jwt,
+    },
+  };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  const getOrders = () => {
+    setIsLoader(true);
+    axios
+      .get(`${BaseURL.PLACE_ORDER}?populate[0]=order_items`, config)
+      .then(res => {
+        console.log('Data ===>', res.data.data);
+        setData(res.data.data);
+
+        setIsLoader(false);
+      })
+      .catch(err => {
+        console.log('Error ====>', err);
+        setIsLoader(false);
+      });
+  };
 
   return (
     <>
       <MyStatusBar backgroundColor={'#0283c3'} />
       <SafeAreaView className={'flex-1 bg-[#F9F9F9]'}>
-        <Header title={'Orders'} CartOnPress={() => navigation.navigate('Cart')}/>
+        <Header
+          title={'Orders'}
+          CartOnPress={() => navigation.navigate('Cart')}
+        />
         <ScrollView
           contentContainerStyle={{
             paddingBottom: height * 0.07,
@@ -46,58 +82,50 @@ const Index = ({navigation, ...props}) => {
             How to log a return:
           </Text>
           <FlatList
-            data={[1, 2, 3, 4, 5, 6]}
-            renderItem={({}) => {
+            data={data}
+            renderItem={({item}) => {
               return (
                 <TouchableOpacity
                   activeOpacity={0.7}
-                  onPress={() => navigation.navigate('OrderDetail')}
+                  // onPress={() => navigation.navigate('OrderDetail')}
                   style={{width: width * 0.95, borderBottomWidth: 1}}
                   className={
                     'flex self-center flex-row justify-between py-3 border-[#D4D4D4] mt-4'
                   }>
                   <View className={'flex flex-row'}>
-                    <View
-                      className={
-                        'h-24 w-24 overflow-hidden bg-red-200 rounded-md mr-2'
-                      }>
-                      <Image
-                        source={Images.dress1}
-                        className={'w-[100%] h-[100%]'}
-                        resizeMode={'stretch'}
-                      />
-                    </View>
                     <View>
                       <Text className={'text-sm text-black font-semibold'}>
-                        Order #0283c3
+                        Order #{item.id}
                       </Text>
                       <Text className={'text-sm text-slate-500 font-semibold'}>
                         Date ordered{' '}
                         <Text className={'text-sm text-black font-semibold'}>
-                          10-02-23
+                          {moment(item.attributes.publishedAt).format(
+                            'MM/DD/YYYY',
+                          )}
                         </Text>
                       </Text>
                       <Text className={'text-sm text-slate-500 font-semibold'}>
                         Item{' '}
                         <Text className={'text-sm text-black font-semibold'}>
-                          2
+                          {item.attributes.order_items?.data?.length}
                         </Text>
                       </Text>
                       <Text className={'text-sm text-slate-500 font-semibold'}>
                         Total{' '}
                         <Text className={'text-sm text-black font-semibold'}>
-                          AED 5,000
+                          AED {item.attributes.totalAmount}
                         </Text>
                       </Text>
                     </View>
                   </View>
                   <View
-                    className={
-                      'rounded-full bg-green-600 flex items-center justify-center h-6 px-4'
-                    }>
+                    className={`rounded-full ${
+                      !item.delivered ? 'bg-slate-500' : 'bg-green-600'
+                    }  flex items-center justify-center h-6 px-4`}>
                     <Text
                       className={'text-xs text-white font-semibold uppercase'}>
-                      Shipped
+                      {!item.attributes.delivered ? 'Pending' : 'Shipped'}
                     </Text>
                   </View>
                 </TouchableOpacity>
@@ -106,6 +134,7 @@ const Index = ({navigation, ...props}) => {
           />
         </ScrollView>
       </SafeAreaView>
+      {isLoader && <Loader />}
       <ReturnLog
         isVisible={modal}
         onPress={() => {
